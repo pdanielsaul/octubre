2428a0e1b56e913ce279848f5630ed5d6ed59354
Your Name <you@example.com>
2013-10-20 22:58:22 -0500

    user scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Octubre::Application.routes.draw do
+  resources :users
+  root to: "users#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
