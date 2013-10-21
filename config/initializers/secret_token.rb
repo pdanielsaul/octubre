b12307709d7939f75dd078219f03a6948396240c
Your Name <you@example.com>
2013-10-20 21:34:57 -0500

    inci

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Octubre::Application.config.secret_key_base = '17b2ed9fd1082db462c0ed1c986c7128536c8939cdd50daaf18222cb604fa99cf8c16d9c2b03ffba54673c1a670e5959ea08f928655dc2eb8c8eed4683a47a11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Octubre::Application.config.session_store :cookie_store, key: '_octubre_session'
