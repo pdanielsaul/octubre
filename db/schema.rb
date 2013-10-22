be69f7da90d912987ab104495da15ca4e9a80204
Your Name <you@example.com>
2013-10-22 00:26:39 -0500

    game scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131021040519) do
+ActiveRecord::Schema.define(version: 20131022025507) do
 
   create_table "games", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
